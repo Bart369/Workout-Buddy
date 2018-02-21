fc3307e94a390a666a7ca1c75ca0206f32c3e7ff
bart369 <bartinpeguero@gmail.com>
2018-02-21 03:39:04 -0500

    import LoginForm into App

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'react-redux'
+import LoginForm from './src/components/LoginForm'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <LoginForm/>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
