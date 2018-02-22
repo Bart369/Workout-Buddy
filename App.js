35e3bab6169aa25929fd6525c6f910bf84b4f0c3
bart369 <bartinpeguero@gmail.com>
2018-02-22 00:21:17 -0500

    Page and Pagesections will hold Inputs

--- App.js
+++ App.js
@@ -6,19 +6,11 @@ import LoginForm from './src/components/LoginForm'
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View >
         <LoginForm/>
-        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { Page, PageSection, Input } from './common'
 
 class LoginForm extends Component {
     render() {
         return (
-            <View>
-                <Text> Hi! </Text>
-            </View>
+            <Page>
+                <PageSection>                    
+                    <Input 
+                        label='Email'
+                        placeholder='email@blahblah.com'           
+                    />                    
+                </PageSection>
+
+                <PageSection>
+                    <Input
+                        label='Password'
+                        placeholder='Anything but 123456789'
+                    />
+                </PageSection>
+            </Page>
         )
     }
 }
