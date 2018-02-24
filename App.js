2815920a7140bd080b82578eb98beb0711e565e5
bart369 <bartinpeguero@gmail.com>
2018-02-24 02:54:06 -0500

    import firebase, actions, reducers & types file

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import firebase from 'firebase'
 import LoginForm from './src/components/LoginForm'
 
 export default class App extends React.Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyAjb_VRx06jsnDVyNrcIKqsnSL6EsyTN1Y",
+      authDomain: "workout-buddy-31b6c.firebaseapp.com",
+      databaseURL: "https://workout-buddy-31b6c.firebaseio.com",
+      projectId: "workout-buddy-31b6c",
+      storageBucket: "workout-buddy-31b6c.appspot.com",
+      messagingSenderId: "722133233706"
+    }
+    firebase.initializeApp(config)
+  }
+
   render() {
+    store = createStore()
+
     return (
-      <View >
+      <Provider store={store}>
+
         <LoginForm/>
-      </View>
+      
+      </Provider>
     );
   }
 }
