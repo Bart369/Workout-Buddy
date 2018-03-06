9946d9cb6603f1195a5823ff1b3c49c46393e8f4
bart369 <bartinpeguero@gmail.com>
2018-03-06 01:58:08 -0500

    reducer comment

--- src/reducers/CardioFormReducer.js
+++ src/reducers/CardioFormReducer.js
@@ -19,3 +19,11 @@ export default (state = INITIAL_STATE, action) => {
             return state
     }
 }
+
+
+// [action.payload.prop] this is key interpolation
+// the key that we are adding to the state object will be determined at 
+// run time. so if we call our action creator with a prop of name that code
+// in the square bracket will become a property of name and look like this 
+// { ...state, name: action.payload.value } which will then update or add
+// a name property in our state object.
